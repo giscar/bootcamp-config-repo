6e3c747fd44f55f4a02d8beb0409e0f8ed364f24
= <carlosleon@MacBook-Pro-de-carlos.local>
2026-02-13 16:46:26 -0500

    install checkstyle

--- customer/src/main/java/com/quarkus/application/service/dto/CustomerResponse.java
+++
@@ -1,7 +0,0 @@
-package com.quarkus.application.service.dto;
-
-public record CustomerResponse(
-        Long id,
-        String name,
-        String email
-) {}
--- customer/src/main/java/com/quarkus/infrastructure/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quarkus.infrastructure.repository;
-
-import com.quarkus.domain.model.Customer;
-import io.quarkus.hibernate.reactive.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class CustomerRepository implements PanacheRepository<Customer> {
-}
